3ae0b33e8a9c3979a531a68aa937f5d31707fcfc
mhenkel2 <mhenkel2@umbc.edu>
2014-05-08 12:13:48 -0400

    added grid system and switching background for night and day system

---
+++ Assets/Scripts/BackgroundSpriteChanger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundSpriteChanger : MonoBehaviour {
+
+	public Sprite nightBackground;
+	public Sprite dayBackground;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameVars.IsNight == true) {
+			this.GetComponent<SpriteRenderer> ().sprite = nightBackground;
+		} else {
+			this.GetComponent<SpriteRenderer> ().sprite = dayBackground;
+		}
+		
+		if (Input.GetKeyDown ("space")) {
+			GameVars.IsNight = !GameVars.IsNight;
+		}
+	}
+}
