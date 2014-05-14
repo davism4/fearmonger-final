9ea12092dba713f371f0758836a134d121d94a6d
kevin <kevin@Kevin-ui-MacBook-Air.local>
2014-05-14 10:48:58 -0400

    Sound Script added & Sounds folder updated
    Sound Script added & Sounds folder updated

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound : MonoBehaviour {
+
+	public AudioClip doorOpen;
+	public AudioClip bgmNight;
+
+	bool isBgmPlayed = false;
+
+	// Use this for initialization
+	void Start () {
+		AudioSource.PlayClipAtPoint (doorOpen, Camera.main.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isBgmPlayed){
+			isBgmPlayed = true;
+			AudioSource.PlayClipAtPoint(bgmNight, Camera.main.transform.position);
+		}
+	}
+}
