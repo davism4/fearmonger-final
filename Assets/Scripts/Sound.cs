3f263f2d471da8f7fb1b74200fb338160b8a53fa
Michael <davism4@umbc.edu>
2014-05-14 18:14:21 -0400

    fixing

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound : MonoBehaviour {
+	public AudioClip bgmDay = Resources.Load<AudioClip>("Sounds/bgm_final_day");
+
+	public void playBgmDay(){
+		if(bgmDay != null)
+			AudioSource.PlayClipAtPoint (bgmDay, Camera.main.transform.position);
+	}
+}
