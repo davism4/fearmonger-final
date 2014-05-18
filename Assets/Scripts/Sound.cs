a0ea249ad75d02671f8b3095243434aaf3879160
kevin <kevin@kevin-ui-air.home>
2014-05-17 23:54:44 -0400

    Rooster sound added when morning comes
    Rooster sound added when morning comes

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -2,17 +2,20 @@
 using System.Collections;
 
 public class Sound : MonoBehaviour{
-	public AudioSource audioSource;
-	public AudioClip sound;
+	private AudioSource audioSource;
+	private AudioClip sound;
+	private AudioClip rooster;
 
 	public void playBgmDay(){
-			audioSource = Camera.main.transform.Find ("Sound").GetComponent<AudioSource> ();
-			sound = Resources.Load<AudioClip> ("Sounds/bgm_final_day");
-			audioSource.Stop ();
-			audioSource.loop = true;
-			audioSource.volume = 1;
-			audioSource.clip = sound;
-			audioSource.Play ();
+		rooster = Resources.Load<AudioClip> ("Sounds/rooster");
+		AudioSource.PlayClipAtPoint (rooster, Camera.main.transform.position);
+		audioSource = Camera.main.transform.Find ("Sound").GetComponent<AudioSource> ();
+		sound = Resources.Load<AudioClip> ("Sounds/bgm_final_day");
+		audioSource.Stop ();
+		audioSource.loop = true;
+		audioSource.volume = 1;
+		audioSource.clip = sound;
+		audioSource.Play ();
 	}
 
 	public void playBgmNight(){
