4ddd2714bb964f9b84c6d9922586947ba67d0cb1
kevin <kevin@kevin-ui-air.home>
2014-05-18 00:11:11 -0400

    Sound script edited
    Sound script edited

--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -4,18 +4,17 @@ using System.Collections;
 public class Sound : MonoBehaviour{
 	private AudioSource audioSource;
 	private AudioClip sound;
-	private AudioClip rooster;
 
 	public void playBgmDay(){
-		rooster = Resources.Load<AudioClip> ("Sounds/rooster");
-		AudioSource.PlayClipAtPoint (rooster, Camera.main.transform.position);
+		sound = Resources.Load<AudioClip> ("Sounds/rooster");
+		AudioSource.PlayClipAtPoint (sound, Camera.main.transform.position);
 		audioSource = Camera.main.transform.Find ("Sound").GetComponent<AudioSource> ();
 		sound = Resources.Load<AudioClip> ("Sounds/bgm_final_day");
 		audioSource.Stop ();
 		audioSource.loop = true;
 		audioSource.volume = 1;
 		audioSource.clip = sound;
-		audioSource.Play ();
+		audioSource.PlayDelayed (1.5f);
 	}
 
 	public void playBgmNight(){
